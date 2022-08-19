ed548ddbd4176d2d9be398b0ef093ab62f0eb8a6
Alexander <alexlimay27@gmail.com>
2022-08-18 23:43:01 -0500

    pizza

---
+++ src/main/java/com/idat/EfAlexanderChinininPizzaApplication.java
@@ -0,0 +1,13 @@
+package com.idat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfAlexanderChinininPizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EfAlexanderChinininPizzaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/idat/model/Cliente.java
@@ -0,0 +1,30 @@
+package com.idat.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity(name ="cliente")
+@Table
+@Data
+@NoArgsConstructor
+public class Cliente {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer idCliente;
+	private String nombreCliente;
+	private String apellidoCliente;
+	private String nroCelular;
+	private String direccion;
+	
+	@ManyToMany(mappedBy = "cliente", cascade = {CascadeType.PERSIST,CascadeType.MERGE}) 
+	private List<Pizza> pizza = new ArrayList<Pizza>();
+}
